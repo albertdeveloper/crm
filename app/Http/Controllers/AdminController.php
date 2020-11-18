c387828e37f53c69574d12f9035f13be1c5b42c2
Albert <rossalbertgonzales@gmail.com>
2020-11-18 23:50:44 +0800

    add AdminController

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\UserRepositoryContract;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    private $userRepository;
+
+    public function __construct(UserRepositoryContract $userRepositoryContract)
+    {
+        $this->userRepository = $userRepositoryContract;
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+
+    public function organisations()
+    {
+        return view('admin.organisations.index');
+    }
+
+    public function users()
+    {
+        return view('admin.users.index');
+    }
+
+    public function profile()
+    {
+        return view('admin.profile',[
+            'user' => $this->userRepository->findUserById(false),
+        ]);
+    }
+
+    public function profile_store(ProfileFormRequest $request)
+    {
+        $this->userRepository->save_profile($request);
+        return redirect()->route('admin.profile');
+    }
+}
