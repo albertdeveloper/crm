d5ed153a121404f1ab4d2ad2118835dcff6baec7
Albert <rossalbertgonzales@gmail.com>
2020-11-21 01:34:57 +0800

    add defaultProfilePicture

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function defaultProfilePicture()
+    {
+        return 'https://ui-avatars.com/api/?name=' . urlencode($this->name) . '&color=7F9CF5&background=EBF4FF&rounded=true';
+    }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -157,8 +157,7 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="{{ asset('AdminLTE-3.0.5/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
-                         alt="User Image">
+                    <img src="{{auth()->user()->defaultProfilePicture()}}"/>
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">{{auth()->user()->name}}</a>
